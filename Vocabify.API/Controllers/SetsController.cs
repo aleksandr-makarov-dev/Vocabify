fc60c6a6931c5e482a741813fe3049c20a5d08bd
Alex <ac5295@student.jamk.fi>
2024-04-27 21:45:35 +0300

    Add project files.

---
+++ Vocabify.API/Controllers/SetsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PuppeteerSharp;
+using System;
+
+namespace Vocabify.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SetsController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public SetsController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet("scrap")]
+        public async Task<FileContentResult> Get([FromQuery] string q)
+        {
+            using var browserFetcher = new BrowserFetcher();
+            await browserFetcher.DownloadAsync();
+            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync(q);
+
+            
+            var stream = await page.PdfDataAsync();
+
+            await browser.CloseAsync();
+
+            return File(stream, "application/pdf","page.pdf");
+
+;        }
+    }
+}
