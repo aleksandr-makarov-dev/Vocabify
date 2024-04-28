fd38474c8142312fbd8747bfde1dca840345c1b2
Alex <ac5295@student.jamk.fi>
2024-04-28 12:32:22 +0300

    create entity related diagram

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Vocabify
+
+## Entity-Related Diagram
+
+```mermaid
+erDiagram
+  USER {
+    Id string PK
+    Email string UK
+    PasswordHash string
+    Image string
+    EmailVerifiedAt date
+  }
+
+  SESSION {
+    Id string PK
+    Token string
+    ExpiresAt date
+    RevokedAt date
+    UserId string FK
+  }
+
+  SET {
+    Id string PK
+    Title string
+    Description string
+    Image string
+    UserId string FK
+    ItemsCount int
+  }
+
+  TERM {
+    Id string PK
+    Text string
+    Definition string
+    Image string
+    TextAudioUrl string
+    DefinitionAudioUrl string
+    SetId string FK
+  }
+
+  USER ||--|{ SET : has
+  USER ||--|{ SESSION : has
+  SET ||--|{ TERM : contains
+```
--- Vocabify.API/Controllers/SetsController.cs
+++ Vocabify.API/Controllers/SetsController.cs
@@ -8,34 +8,25 @@ namespace Vocabify.API.Controllers
     [ApiController]
     public class SetsController : ControllerBase
     {
-        private readonly IWebHostEnvironment _env;
 
-        public SetsController(IWebHostEnvironment env)
-        {
-            _env = env;
-        }
-
-        [HttpGet("scrap")]
-        public async Task<FileContentResult> Get([FromQuery] string q)
+        [HttpGet("scrapper/puppeteer")]
+        public async Task<FileContentResult> GetPuppeteer([FromQuery] string q = "https://quizlet.com/ru/653463426/sm2-kpl-7-koulutus-opiskelu-flash-cards")
         {
             await new BrowserFetcher().DownloadAsync();
             var browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
                 Headless = true,
-                Args = new string[] { "--no-sandbox", "--disable-setuid-sandbox" }
+                Args = ["--no-sandbox", "--disable-setuid-sandbox"]
             });
 
-            Console.WriteLine("Browser launched successfully.");
             var page = await browser.NewPageAsync();
             await page.GoToAsync(q);
-
             
             var stream = await page.PdfDataAsync();
 
             await browser.CloseAsync();
 
             return File(stream, "application/pdf","page.pdf");
-
-;        }
+        }
     }
 }
